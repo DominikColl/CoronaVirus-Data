9bc3ef573042b1389498bb8e97bd314e4a99e24b
DominikColl <DAColl@student.fullsail.edu>
2020-06-25 22:14:01 -0700

    Build file

---
+++ build/precache-manifest.e8010b8d2ecc4d55bdf84f2095fb20b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93a13aae0eb4c4f5396325ecf604874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52feb9c0bd230698ff86",
+    "url": "/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "a73ad5605183510400cc",
+    "url": "/static/css/main.46f429d2.chunk.css"
+  },
+  {
+    "revision": "52feb9c0bd230698ff86",
+    "url": "/static/js/2.c1b4478a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c1b4478a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a73ad5605183510400cc",
+    "url": "/static/js/main.f7500441.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
